77dafd1ea1a281e74e8883c4ac1e8ae04016a950
5o5_x250 <ante.sekelez@gmail.com>
2021-08-14 20:38:30 +0200

    mali laptop salje s 2 brancha

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // test git hub comment
+        // calc divide test git 
     }
 }
